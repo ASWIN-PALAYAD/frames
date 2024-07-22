b0d823e1448fdf6e235beac2b199fd04557293cf
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-07-22 10:32:51 +0530

    sidebar completed

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,84 @@
+import './sidebar.css'
+import {RssFeed,Chat,PlayCircle,Groups,Bookmark,HelpOutline,WorkOutline,School,Event} from '@mui/icons-material';
+
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+              <RssFeed className='sidebarIcon' />
+              <span className='sidebarListItemText'>Feed</span>
+          </li>
+          <li className="sidebarListItem">
+              <Chat className='sidebarIcon' />
+              <span className='sidebarListItemText'>Chat</span>
+          </li>
+          <li className="sidebarListItem">
+              <PlayCircle className='sidebarIcon' />
+              <span className='sidebarListItemText'>Videos</span>
+          </li>
+          <li className="sidebarListItem">
+              <Groups className='sidebarIcon' />
+              <span className='sidebarListItemText'>Groups</span>
+          </li>
+          <li className="sidebarListItem">
+              <Bookmark className='sidebarIcon' />
+              <span className='sidebarListItemText'>Bookmarks</span>
+          </li>
+          <li className="sidebarListItem">
+              <HelpOutline className='sidebarIcon' />
+              <span className='sidebarListItemText'>Questions</span>
+          </li>
+          <li className="sidebarListItem">
+              <WorkOutline className='sidebarIcon' />
+              <span className='sidebarListItemText'>Job</span>
+          </li>
+          <li className="sidebarListItem">
+              <Event className='sidebarIcon' />
+              <span className='sidebarListItemText'>Events</span>
+          </li>
+          <li className="sidebarListItem">
+              <School className='sidebarIcon' />
+              <span className='sidebarListItemText'>Courses</span>
+          </li>
+        </ul>
+        <button className='sidebarButton'>
+          show more
+        </button>
+        <hr className='sidebarHr'/>
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className='SidebarFriendName' >Sherin</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className='SidebarFriendName' >Sherin</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className='SidebarFriendName' >Sherin</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className='SidebarFriendName' >Sherin</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className='SidebarFriendName' >Sherin</span>
+          </li><li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className='SidebarFriendName' >Sherin</span>
+          </li><li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className='SidebarFriendName' >Sherin</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
-import Topbar from '../../components/Topbar'
-import './home.css'
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topbar from "../../components/topbar/Topbar";
+import "./home.css";
 
 const Home = () => {
   return (
-    <Topbar/>
-  )
-}
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
