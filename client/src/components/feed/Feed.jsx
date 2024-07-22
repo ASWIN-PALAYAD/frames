f06a08cceb583946c40f4e8a25818dcff93ea8ae
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-07-22 15:15:39 +0530

    UI share field completed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,9 +1,14 @@
+import Share from '../share/Share'
 import './feed.css'
 
 const Feed = () => {
   return (
-    <div className='feed'>Feed</div>
+    <div className='feed'>
+      <div className="feedWrapper">
+        <Share/>
+      </div>
+    </div>
   )
 }
  
-export default Feed
+export default Feed 
