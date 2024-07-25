d6d8ca474088dcf2cf72ec0f7b1b51568aa51c2d
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-07-25 11:48:20 +0530

    login completed

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,29 +1,61 @@
-import './login.css'
+import { useContext, useRef } from "react";
+import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@mui/material";
 
 const Login = () => {
-  return (
-    <div className='login'>
-        <div className="loginWrapper">
-            <div className="loginLeft">
-                <h3 className='loginLogo'>Frames</h3>
-                <span className="loginDesc">
-                    Connect with friends and the world around you on Frames..
-                </span>
-            </div>
-            <div className="loginRight">
-                <div className="loginBox">
-                    <input type="text" className="loginInput" placeholder='Email'/>
-                    <input type="text" className="loginInput" placeholder='Password'/>
-                    <button className='loginButton'>Login</button>
-                    <span className='loginForgot'>Forgot Password</span>
-                    <button className='loginRegisterButton'>
-                        Create a New Account
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+  const email = useRef();
+  const password = useRef();
 
-export default Login
+  const { user, isFetching, dispatch } = useContext(AuthContext);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+  };
+  console.log(user);
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Frames</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on Frames..
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              type="email"
+              className="loginInput"
+              ref={email}
+              required
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              className="loginInput"
+              ref={password}
+              required
+              placeholder="Password"
+            />
+            <button className="loginButton" disabled={isFetching}>
+              {isFetching ? <CircularProgress /> : "Login"}
+            </button>
+            <span className="loginForgot">Forgot Password</span>
+            <button className="loginRegisterButton">
+            {isFetching ? <CircularProgress /> : "Create new account"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
